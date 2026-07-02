f8877269b1a9597cdb32fe008230e45aca86f7cb
aaravrangayan-hash <aaravrangayan@gmail.com>
2026-07-01 20:27:06 -0700

    Update state_graph.py

--- core/state_graph.py
+++ core/state_graph.py
@@ -1,16 +1,26 @@
-import time
-
 class StateGraph:
-    def __init__(self):
-        self.state = {}
-        self.history = []
+    def __init__(self, logger=None):
+        self.state = "IDLE"
+        self.transitions = {}
+        self.logger = logger
 
-    def update(self, key, value):
-        self.state[key] = value
-        self.history.append((time.time(), key, value))
+    def add_transition(self, from_state, condition, to_state):
+        self.transitions.setdefault(from_state, []).append((condition, to_state))
 
-    def get(self, key, default=None):
-        return self.state.get(key, default)
+    def update(self, input_signal):
+        old_state = self.state
 
-    def snapshot(self):
-        return dict(self.state)
+        for condition, next_state in self.transitions.get(self.state, []):
+            if condition(input_signal):
+                self.state = next_state
+
+                if self.logger:
+                    self.logger.log_state_change(
+                        old_state,
+                        self.state,
+                        f"Triggered by input: {input_signal}"
+                    )
+                break
+
+    def get_state(self):
+        return self.state
